f52f2f3fc3117463210553878eca6946474949c0
mathy <matiwordgit@gmail.com>
2024-06-21 19:16:26 -0300

    Finalizado parte Materia

---
+++ src/main/java/ar/edu/unju/fi/controller/MateriaController.java
@@ -0,0 +1,70 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.DTO.MateriaDTO;
+import ar.edu.unju.fi.service.MateriaService;
+import ar.edu.unju.fi.model.Materia;
+
+@Controller
+public class MateriaController {
+    
+    @Autowired
+    MateriaService materiaService;
+    
+    @Autowired
+    MateriaDTO nuevaMateriaDTO;
+
+    @GetMapping("/formularioMateria")
+    public ModelAndView getFormMateria() {
+        ModelAndView modelView = new ModelAndView("formMateria");
+        modelView.addObject("nuevaMateria", nuevaMateriaDTO);
+        return modelView;
+    }
+    
+    @GetMapping("/ListaMaterias")
+    public ModelAndView getListaMateria() {
+        ModelAndView modelView = new ModelAndView("listaDeMaterias");
+        modelView.addObject("listadoMateria", materiaService.mostrarMaterias());
+        return modelView;
+    }
+
+    @PostMapping("/guardarMateria")
+    public ModelAndView saveMateria(@ModelAttribute("nuevaMateria") MateriaDTO materiaParaGuardar) {
+        materiaService.guardarMateria(materiaParaGuardar);
+        ModelAndView modelView = new ModelAndView("listaDeMaterias");
+        modelView.addObject("listadoMateria", materiaService.mostrarMaterias());
+        return modelView;
+    }
+
+    @GetMapping("/quitarMateria/{codigo}")
+    public ModelAndView deleteMateriaListado(@PathVariable(name="codigo") String codigo) {
+        materiaService.borrarMateria(codigo);
+        ModelAndView modelView = new ModelAndView("listaDeMaterias");
+        modelView.addObject("listadoMateria", materiaService.mostrarMaterias());
+        return modelView;
+    }
+
+    @GetMapping("/modificarMateria/{codigo}")
+    public ModelAndView getModificarMateria(@PathVariable(name="codigo") String codigo) {
+        Materia materiaParaModificar = materiaService.buscarMateria(codigo);
+        ModelAndView modelView = new ModelAndView("formMateria");
+        modelView.addObject("nuevaMateria", materiaParaModificar);
+        return modelView;
+    }
+
+    @PostMapping("/modificarMateria")
+    public ModelAndView updateMateria(@ModelAttribute("nuevaMateria") MateriaDTO materiaModificada) {
+        materiaService.modificarMateria(materiaModificada);
+        ModelAndView modelView = new ModelAndView("listaDeMaterias");
+        modelView.addObject("listadoMateria", materiaService.mostrarMaterias());
+        return modelView;
+    }
+}
+
---
+++ src/main/java/ar/edu/unju/fi/service/imp/MateriaServiceImp.java
@@ -0,0 +1,71 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.DTO.MateriaDTO;
+import ar.edu.unju.fi.model.Materia;
+import ar.edu.unju.fi.repository.MateriaRepository;
+import ar.edu.unju.fi.service.MateriaService;
+
+@Service
+public class MateriaServiceImp implements MateriaService {
+
+    @Autowired
+    MateriaRepository materiaRepository;
+
+    @Override
+    public void guardarMateria(MateriaDTO materiaDTO) {
+        Materia materia = convertirAEntidad(materiaDTO);
+        materia.setEstado(true);
+        materiaRepository.save(materia);
+    }
+
+    @Override
+    public List<Materia> mostrarMaterias() {
+        return materiaRepository.findMateriaByEstado(true);
+    }
+
+    @Override
+    public void borrarMateria(String codigo) {
+        Optional<Materia> optionalMateria = materiaRepository.findById(codigo);
+        if (optionalMateria.isPresent()) {
+            Materia materia = optionalMateria.get();
+            materia.setEstado(false);
+            materiaRepository.save(materia);
+        }
+    }
+
+    @Override
+    public void modificarMateria(MateriaDTO materiaDTO) {
+        Optional<Materia> optionalMateria = materiaRepository.findById(materiaDTO.getCodigo());
+        if (optionalMateria.isPresent()) {
+            Materia materia = optionalMateria.get();
+            materia.setNombre(materiaDTO.getNombre());
+            materia.setCurso(materiaDTO.getCurso());
+            materia.setCantidadHoras(materiaDTO.getCantidadHoras());
+            materia.setModalidad(materiaDTO.getModalidad());
+            materiaRepository.save(materia);
+        }
+    }
+
+    @Override
+    public Materia buscarMateria(String codigo) {
+        return materiaRepository.findById(codigo).orElse(null);
+    }
+
+    private Materia convertirAEntidad(MateriaDTO materiaDTO) {
+        Materia materia = new Materia();
+        materia.setCodigo(materiaDTO.getCodigo());
+        materia.setNombre(materiaDTO.getNombre());
+        materia.setCurso(materiaDTO.getCurso());
+        materia.setCantidadHoras(materiaDTO.getCantidadHoras());
+        materia.setModalidad(materiaDTO.getModalidad());
+        return materia;
+    }
+}
+
