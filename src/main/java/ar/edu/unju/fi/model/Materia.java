a88d814d6813b97fa4c42df244f96486bad7b933
AgusTorito <110627871+AgusTorito@users.noreply.github.com>
2024-06-22 17:48:45 -0300

    Update Materia.java

--- src/main/java/ar/edu/unju/fi/model/Materia.java
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -2,8 +2,6 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -11,10 +9,9 @@ import lombok.Data;
 @Component
 @Entity
 public class Materia {
-    
-    
-	@Id private String codigo;
-    
+	
+    @Id 
+    private String codigo;
     private String nombre;
     private String curso;
     private int cantidadHoras;
