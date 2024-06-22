100d9de99b10b028742bebcbbd2eef735537a1db
AgusTorito <110627871+AgusTorito@users.noreply.github.com>
2024-06-22 17:50:11 -0300

    Update MateriaRepository.java

--- src/main/java/ar/edu/unju/fi/repository/MateriaRepository.java
+++ src/main/java/ar/edu/unju/fi/repository/MateriaRepository.java
@@ -1,8 +1,6 @@
 package ar.edu.unju.fi.repository;
 
 import java.util.List;
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ar.edu.unju.fi.model.Materia;
